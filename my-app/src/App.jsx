5a0352a9e456298a2ac5d4f92c660a4ba2dae08c
Saadeddine El Hajjaji <sa.elhajjaji@aui.ma>
2025-07-16 15:58:46 +0000

    added some styling and made some changes to the way components are rendered

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,20 +1,19 @@
 import Header from './components/Header.jsx'
 import Entry from './components/Entry.jsx'
-import img from './assets/mountfuji.avif'
+import data from './data.js'
 
 function App() {
+  const entries = data.map(entry => (
+    <Entry
+      key={entry.id}
+      {...entry}
+    />
+  ))
+
   return (
     <>
       <Header />
-      <Entry
-        img={{ src: img, alt: "Mount Fuji" }}
-        country="Japan"
-        title="Mount Fuji"
-        googleMapsLink="https://www.google.com/maps/place/Mont+Fuji/@35.3606233,138.767638,14z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D"
-        dates="12 Jan, 2021 - 24 Jan, 2021"
-        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-        Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-      />
+      {entries}
     </>
   )
 }
--- my-app/src/components/Entry.jsx
+++ my-app/src/components/Entry.jsx
@@ -1,8 +1,8 @@
-export default function Entry({img, country, title, googleMapsLink, dates, text}) {
+export default function Entry(props) {
     return (
         <div className="entry">
             <div className="entry-img-container">
-                <img src={img.src} alt={img.alt}/>
+                <img src={props.img.src} alt={props.img.alt}/>
             </div>
             <div className="entry-info-container">
                 <div className="entry-location-info">
@@ -10,16 +10,16 @@ export default function Entry({img, country, title, googleMapsLink, dates, text}
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F55A5A" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
                         </svg>
-                        <span className="entry-country-name">{country}</span>
+                        <span className="entry-country-name">{props.country.toUpperCase()}</span>
                     </div>
-                    <a href={googleMapsLink} target="_blank">
+                    <a href={props.googleMapsLink} target="_blank">
                         View on Google Maps
                     </a>
                 </div>
-                <h1 className="entry-location-name">{title}</h1>
-                <h1 className="entry-date">{dates}</h1>
+                <h1 className="entry-location-name">{props.title}</h1>
+                <h1 className="entry-date">{props.dates}</h1>
                 <p className="entry-description">
-                    {text}
+                    {props.text}
                 </p>
             </div>
         </div>
