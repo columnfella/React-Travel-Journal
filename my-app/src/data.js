942a12578aa1602d2ce13420b17cd98fadca0e48
Saadeddine El Hajjaji <sa.elhajjaji@aui.ma>
2025-07-16 16:18:14 +0000

    final commit

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -35,4 +35,16 @@ export default [
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
+    {
+        id: 4,
+        img: {
+            src: "https://images.unsplash.com/photo-1716220907028-73fc90b45cbd?q=80&w=663&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Tokyo Tower"
+        },
+        title: "Tokyo Tower",
+        country: "Japan",
+        googleMapsLink: "https://www.google.com/local/place/fid/0x60188bbd9009ec09:0x481a93f0d2a409dd/photosphere?iu=https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid%3DMt09GeGiV1TgMoiM26Kz6g%26cb_client%3Dsearch.gws-prod.gps%26yaw%3D264.63373%26pitch%3D0%26thumbfov%3D100%26w%3D0%26h%3D0&ik=CAISFk10MDlHZUdpVjFUZ01vaU0yNkt6Nmc%3D&sa=X&ved=2ahUKEwi64ODw1cGOAxVqVEEAHcmkOwAQpx96BAgZEBI",
+        dates: "07 Dec, 2021 - 14 Dec, 2021",
+        text: "Tokyo Tower (東京タワー, Tōkyō Tawā; pronounced [toːkʲoː taɰᵝaː] ⓘ), also known as the Japan Radio Tower (日本電波塔, Nippon denpatō) is a communications and observation tower in the district of Shiba-koen in Minato, Tokyo, Japan, completed in 1958."
+    }
 ]
